cdd0fbe1e53b40ee7f84ebb4292da444f5862f8e
Anh Lam Truong <85408593+anhlamtruong@users.noreply.github.com>
2024-02-18 18:09:38 -0500

    fixing store_post api

--- app/api/admin/store/route.ts
+++ app/api/admin/store/route.ts
@@ -1,4 +1,5 @@
 import { UserRole } from "@/generated/authenticate/@prisma-client-authenticate";
+import { Prisma } from "@/generated/cookit-ecommerce-service/@prisma-client-cookit-ecommerce-service";
 import { currentUser } from "@/lib/auth";
 import prismaStore from "@/lib/service/prisma_store";
 import { NextResponse } from "next/server";
@@ -28,6 +29,20 @@ export async function POST(req: Request) {
         status: 400,
       });
     }
+    if (!chefId) {
+      return new NextResponse("ChefId of the Store is required", {
+        status: 400,
+      });
+    }
+    const existingStore = await prismaStore.store.findUnique({
+      where: {
+        name: name,
+      },
+    });
+
+    if (existingStore) {
+      return new NextResponse("Store name already in use", { status: 409 }); // 409 Conflict
+    }
 
     const store = await prismaStore.store.create({
       data: {
@@ -44,6 +59,17 @@ export async function POST(req: Request) {
 
     return NextResponse.json(store);
   } catch (error) {
+    if (error instanceof Prisma.PrismaClientKnownRequestError) {
+      // Check for unique constraint violation
+      if (error.code === "P2002") {
+        return new NextResponse(
+          `A store with the name "${name}" already exists.`,
+          {
+            status: 409,
+          }
+        );
+      }
+    }
     console.log("[STORES_POST]", error);
     return new NextResponse("API ERROR", {
       status: 500,
