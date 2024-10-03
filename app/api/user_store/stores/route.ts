bd97dd300f34372a964075c175e0d2a65d058186
BAU Registrar <registrar@bau.edu>
2024-10-03 17:05:40 -0400

    hotfix

--- app/api/user_store/stores/route.ts
+++ app/api/user_store/stores/route.ts
@@ -2,10 +2,11 @@ import prismaStore from "@/lib/service/prisma_store";
 import { NextResponse } from "next/server";
 export async function GET(req: Request) {
   try {
-    const stores = await prismaStore.store.findMany();
-
+    const stores = await prismaStore.store.findMany({ take: 10 });
+    console.log(stores);
     return NextResponse.json(stores);
   } catch (error) {
+    console.error("Error fetching stores:", error);
     return new NextResponse("API ERROR", {
       status: 500,
       statusText: "Internal Server Error : GET STORES",
